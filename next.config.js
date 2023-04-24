38d983cb5e47812a54e8709982039b8e7e16311b
mehdi <hamar3106@gmail.com>
2023-04-24 16:25:33 +0100

    fixing ClickJacker vulnerabilty

--- next.config.js
+++ next.config.js
@@ -1,40 +1,57 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = (phase, { defaultConfig }) => {
-	return {
-		reactStrictMode: true,
-		swcMinify: true,
-		images: {
-			domains: ['cdn.shopify.com' , '*']
-		},
-		experimental: {
-			// appDir: true
-			// runtime: 'experimental-edge'
-		},
-		redirects: async () => {
-			return [
-				{
-					source: '/pages/contact',
-					destination: '/contact-us',
-					permanent: true
-				},
-				{
-					source: '/collections/frontpage',
-					destination: '/',
-					permanent: true
-				},
-				{
-					source: '/collections/all',
-					destination: '/',
-					permanent: true
-				},
-				{
-					source: '/collections',
-					destination: '/',
-					permanent: true
-				}
-			];
-		}
-	};
+  return {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ["cdn.shopify.com", "*"],
+    },
+    experimental: {
+      // appDir: true
+      // runtime: 'experimental-edge'
+    },
+    redirects: async () => {
+      return [
+        {
+          source: "/pages/contact",
+          destination: "/contact-us",
+          permanent: true,
+        },
+        {
+          source: "/collections/frontpage",
+          destination: "/",
+          permanent: true,
+        },
+        {
+          source: "/collections/all",
+          destination: "/",
+          permanent: true,
+        },
+        {
+          source: "/collections",
+          destination: "/",
+          permanent: true,
+        },
+      ];
+    },
+    headers: async () => {
+      return [
+        {
+          source: "/",
+          headers: [
+            {
+              key: "X-Frame-Options",
+              value: "SAMEORIGIN",
+            },
+            {
+              key: "Content-Security-Policy",
+              value: "frame-ancestors 'self'",
+            },
+          ],
+        },
+      ];
+    },
+  };
 };
 
 module.exports = nextConfig;
